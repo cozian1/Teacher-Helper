9ed1b5d11e66859dc29b5d9b627e12a5edde477b
Susant Dash <susantdash0010@gmail.com>
2021-08-01 21:08:28 +0530

    First

--- app/src/main/java/com/spectrum/task3/Activities/Timetable.java
+++ app/src/main/java/com/spectrum/task3/Activities/Timetable.java
@@ -5,12 +5,20 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.spectrum.task3.Adapter.DayAdapter;
 import com.spectrum.task3.Adapter.EventAdapter;
 import com.spectrum.task3.ModelClasses.DayList;
 import com.spectrum.task3.ModelClasses.Mylist;
 import com.spectrum.task3.ModelClasses.ReminderList;
+import com.spectrum.task3.ModelClasses.TODO;
 import com.spectrum.task3.ModelClasses.TimetableAdapter;
 import com.spectrum.task3.R;
 
@@ -26,6 +34,8 @@ public class Timetable extends AppCompatActivity {
    RecyclerView recyclerView;
    DayAdapter myAdapter0;
    TimetableAdapter myAdapter1;
+   DatabaseReference databaseReference;
+
    List<Mylist> monday=new ArrayList<>();
    List<Mylist> thuday=new ArrayList<>();
    List<Mylist> wedday=new ArrayList<>();
@@ -38,6 +48,9 @@ public class Timetable extends AppCompatActivity {
       super.onCreate(savedInstanceState);
       setContentView(R.layout.activity_timetable);
       getSupportActionBar().hide();
+
+      databaseReference = FirebaseDatabase.getInstance().getReference().child(FirebaseAuth.getInstance().getCurrentUser().getUid().toString()).child("Timetable");
+
 
       recyclerView =findViewById(R.id.dayview);
       daylist=new ArrayList<>();
@@ -72,11 +85,12 @@ public class Timetable extends AppCompatActivity {
       recyclerView.setLayoutManager(new LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false));
       recyclerView.setAdapter(myAdapter0);
       myAdapter0.setData(daylist,Timetable.this);
+      setday(currentday);
 
    }
    public void setday(String s){
       RecyclerView rv=findViewById(R.id.mylist);
-      rv.setLayoutManager(new LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false));
+      rv.setLayoutManager(new LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false));
       rv.setAdapter(myAdapter1);
       switch (s){
          case "MONDAY":
@@ -95,5 +109,33 @@ public class Timetable extends AppCompatActivity {
             myAdapter1.setData(sunday);break;
       }
       myAdapter1.notifyDataSetChanged();
+   }
+
+
+   private void addfdata(Mylist t) {
+      databaseReference.child(currentday).child(databaseReference.push().getKey()).setValue(t);
+   }
+
+   private void getfdata( List<Mylist> l){
+      databaseReference.child(currentday).addValueEventListener(new ValueEventListener() {
+         @Override
+         public void onDataChange(DataSnapshot dataSnapshot) {
+            if(l.size()==0) return;
+            for(DataSnapshot dataSnapshot1: dataSnapshot.getChildren())
+            {
+               Mylist p=dataSnapshot1.getValue(Mylist.class);
+               l.add(p);
+            }
+         }
+         @Override
+         public void onCancelled(DatabaseError databaseError) {
+            Toast.makeText(getApplicationContext(), "No Data", Toast.LENGTH_SHORT).show();
+         }
+      });
+   }
+   private void updatefdata(List<Mylist> l){
+      databaseReference.child(currentday).removeValue();
+      for(Mylist t:l)
+         databaseReference.child(currentday).child(databaseReference.push().getKey()).setValue(t);
    }
 }
